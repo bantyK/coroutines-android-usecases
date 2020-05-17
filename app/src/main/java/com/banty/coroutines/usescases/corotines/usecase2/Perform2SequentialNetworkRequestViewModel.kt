42386ef9d875c7b2dccb882bc507c5941c45ae3c
bantyK <bantyp92@gmail.com>
2020-05-17 18:47:48 +0530

    test commit

---
+++ app/src/main/java/com/banty/coroutines/usescases/corotines/usecase2/Perform2SequentialNetworkRequestViewModel.kt
@@ -0,0 +1,8 @@
+package com.banty.coroutines.usescases.corotines.usecase2
+
+import com.banty.coroutines.base.BaseViewModel
+import com.banty.coroutines.usescases.corotines.usecase1.UiState
+
+class Perform2SequentialNetworkRequestViewModel : BaseViewModel<UiState>() {
+
+}
